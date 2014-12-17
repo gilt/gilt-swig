6cf09e6dfad2fb11bb743bb5f7e3371a367a9054
Rory Haddon <rhaddon@gilt.com>
2014-12-17 10:45:33 +0000

    Fix swig install handling of config placeholders. Also fix incorrect regex

--- tasks/swig-install/templates/main.js
+++ tasks/swig-install/templates/main.js
@@ -3,7 +3,7 @@
   var oldRequireCallback = require.callback;
   require.callback = function requireCallback () {
     window.createConfig = function createConfig (key, value) {
-      (define || createModule)(key.replace(/\\./g, "/"), function () { return value; });
+      (define || createModule)(key.replace(/\./g, "/"), function () { return value; });
     };
     if (oldRequireCallback) { oldRequireCallback(); }
   };
